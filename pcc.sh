cc6cd3214974d8d0949eeb6362f3a30178d94300
Shellgate <128194280+Shellgate@users.noreply.github.com>
2023-12-06 07:13:56 +0330

    Update pcc.sh

--- pcc.sh
+++ pcc.sh
@@ -5,13 +5,20 @@ project_name="TCP Optimization PCC"
 backup_and_destination_path="/etc/sysctl.conf"
 new_file_url="https://raw.githubusercontent.com/Shellgate/pcc-tcp-optimizer/main/sysctl.conf"
 
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 # Display system information
 display_system_info() {
-    echo -e "=== $project_name ==="
-    echo -e "System Information:"
-    echo -e "  OS: $(lsb_release -d | cut -f2)"
-    echo -e "  CPU: $(grep 'model name' /proc/cpuinfo | head -n1 | cut -d' ' -f3-)"
-    echo -e "  RAM: $(free -m | awk '/Mem/ {print $2 " MB"}')"
+    echo -e "\n=== ${GREEN}$project_name${NC} ==="
+    echo -e "${RED}System Information:${NC}"
+    echo -e "+------------------------+"
+    echo -e "|  OS: $(lsb_release -d | cut -f2)  |"
+    echo -e "|  CPU: $(grep 'model name' /proc/cpuinfo | head -n1 | cut -d' ' -f3-)  |"
+    echo -e "|  RAM: $(free -m | awk '/Mem/ {print $2 " MB"}')  |"
+    echo -e "+------------------------+"
 }
 
 # Prompt user for restart
@@ -38,20 +45,20 @@ select option in "${options[@]}"; do
             cp "$backup_and_destination_path" "$backup_and_destination_path.bak"
             echo "Downloading the new file..."
             curl -sSfL "$new_file_url" -o "$backup_and_destination_path" --progress-bar
-            echo "File replaced, backup created."
+            echo -e "${GREEN}File replaced, backup created.${NC}"
             prompt_restart
             break
             ;;
         2)
             # Option 2: Restore Initial Backup
             cp "$backup_and_destination_path.bak" "$backup_and_destination_path"
-            echo "File restored from initial backup."
+            echo -e "${GREEN}File restored from initial backup.${NC}"
             prompt_restart
             break
             ;;
         3)
             # Option 3: Exit
-            echo "Exiting."
+            echo -e "${RED}Exiting.${NC}"
             break
             ;;
         *) echo "Invalid choice. Please select again." ;;
